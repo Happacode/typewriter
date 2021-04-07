b4c00c9b0e9471f81c69e037220e8bed84b995cb
Harp Sandhu <happacode@protonmail.com>
2021-04-06 23:50:35 -0700

    D2 files

---
+++ hello_timeout.js
@@ -0,0 +1,17 @@
+function sayHi() {
+  console.log('4311o');
+};
+
+function sayThree() {
+  console.log('th3r3');
+};
+
+function sayWorld() {
+  console.log('w0r1d');
+};
+
+setTimeout(sayHi, 1000);
+setTimeout(sayThree, 2000);
+setTimeout(sayWorld, 3000);
+
+console.log('I am done being programmed.');
